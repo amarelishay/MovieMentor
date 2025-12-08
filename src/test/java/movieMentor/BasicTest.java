52eeadd89ee6209fa506b36a975957eba0453b18
amarelishay <96876947+amarelishay@users.noreply.github.com>
2025-12-07 23:52:44 -0800

    Add basic JUnit test

---
+++ src/test/java/movieMentor/BasicTest.java
@@ -0,0 +1,11 @@
+package movieMentor;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BasicTest {
+    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }
+}
